ad44af80419368c1e6c6315c5c225e106825a92d
Daiki Suzuki <daikiszk@amazon.co.jp>
2024-03-27 16:40:20 +0900

    fix: システムコンテキスト保存のデグレ修正

--- packages/web/src/prompts/index.ts
+++ packages/web/src/prompts/index.ts
@@ -67,6 +67,12 @@ export type PromptList = {
   experimental?: boolean;
 }[];
 
+export type SystemContextListItem = {
+  systemContextTitle: string;
+  systemContext: string;
+  systemContextId: string;
+};
+
 export interface Prompter {
   systemContext(pathname: string): string;
   chatPrompt(params: ChatParams): string;
